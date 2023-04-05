81526dc4b7d1bf5f8f22f99dbf28965ab92605f7
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-04-05 17:57:00 +0200

    build: move to scala 3.*; drop scala mock

--- 03-scalatest/build.gradle.kts
+++ 03-scalatest/build.gradle.kts
@@ -9,16 +9,18 @@ repositories {
     mavenCentral()
 }
 
+scoverage {
+    this.scoverageVersion.set("2.0.7")
+}
+
 dependencies {
-    val scalaVersion = "2.13.8"
-    val (scalaMinor, _) = requireNotNull(Regex("^(\\d+\\.\\d+)(\\.\\d+)?$").matchEntire(scalaVersion)).destructured
-    implementation("org.scala-lang:scala-library:$scalaVersion")
-    testImplementation("org.scalatest:scalatest_$scalaMinor:3.3.0-SNAP3")
-    testImplementation("org.scalamock:scalamock_$scalaMinor:5.2.0")
-    testRuntimeOnly("com.vladsch.flexmark:flexmark-profile-pegdown:0.36.8")
-    val scoverageVersion = "1.4.11"
-    scoverage("org.scoverage:scalac-scoverage-plugin_$scalaVersion:$scoverageVersion")
-    scoverage("org.scoverage:scalac-scoverage-runtime_$scalaMinor:$scoverageVersion")
+    val scalaVersion = "3.2.2"
+    val crossWithScala2 = "2.13"
+    val (scalaMinor, _) = requireNotNull(Regex("^(\\d+)(\\.\\d+)(\\.\\d+)?$").matchEntire(scalaVersion)).destructured
+    // https://mvnrepository.com/artifact/org.scala-lang/scala3-library
+    implementation("org.scala-lang:scala3-library_3:${scalaVersion}")
+    testImplementation("org.scalatest:scalatest_$scalaMinor:3.2.12")
+    testRuntimeOnly("com.vladsch.flexmark:flexmark-all:0.64.0")
 }
 
 // Force a scoverage report if check is requested
